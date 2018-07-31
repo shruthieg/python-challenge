2523a7129c5541520314c129e50cb7e4f10b38ce
shruthi elkal <38443443+shruthieg@users.noreply.github.com>
2018-07-31 11:13:01 -0500

    final result

---
+++ PyBank/main.py
@@ -0,0 +1,37 @@
+#main code here
+import csv
+import os
+import math
+
+from pathlib import Path
+filepath = Path("C:\\Users\Shruthi\\python-challenge\\PyBank\\budget_data.csv")
+with open(filepath, newline="",encoding='utf-8') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+    months = []
+    revenue = []
+    change =[]
+    next(csvreader)
+    for row in csvreader:
+        months.append(row[0])
+        revenue.append(int(row[1]))
+    print("FINANCIAL ANALYSIS")
+    print("total number of months = "+str(len(months)))
+    print("total net amount of profit/losses = " +str(sum(revenue)))
+    for i in range(1,len(revenue)):
+        change.append(revenue[i] - revenue[i-1])
+    print("change between months = "+str(change))
+    print("average change =" +str(sum(change)/(len(months)-1)))
+    print(max(change))
+    print(months[(change.index(max(change)))+1])
+    print("greatest increase in profits (date and amount) over the entire period : " +str(months[(change.index(max(change)))+1]) +  ":"  +str(max(change)))
+    print(min(change))  
+    print(months[(change.index(min(change)))+1])
+    print("greatest increase in profits (date and amount) over the entire period : " +str(months[(change.index(min(change)))+1]) +  ": ("  +str(min(change))+")") 
+       
+       
+        #row1(0)=0
+    #print("average change in Profit/Losses between months over the peiod"
+
+       #print(string.count(row[0]))
+       #print(string.count("geeks"))
+
